abd38bb3a1a7718f5a561575957037626561cc3b
Amir Salehi <isamirsalehi@gmail.com>
2021-03-12 15:06:04 +0330

    .env config implemented

---
+++ bootstrap/init.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASE_PATH', __DIR__ . '/../');
+
+include BASE_PATH .  'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 
-
-echo $_SERVER['REQUEST_URI'];
+include 'bootstrap/init.php';
