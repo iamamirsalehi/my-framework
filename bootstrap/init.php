7a0506729a315008e962c3d5c950a38c38ac0fc3
Amir Salehi <isamirsalehi@gmail.com>
2021-03-12 15:13:52 +0330

    Helper added

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -4,5 +4,7 @@ define('BASE_PATH', __DIR__ . '/../');
 
 include BASE_PATH .  'vendor/autoload.php';
 
+include BASE_PATH .  'helpers/helper.php';
+
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
